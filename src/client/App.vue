5f275d5a2f7fe6ef865cef081ffd7640d8065e82
Steve Warren <steve-warren@users.noreply.github.com>
2023-07-06 18:17:52 -0500

    add vite + vue + tailwind css, .gitignore file

---
+++ src/client/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </template>
