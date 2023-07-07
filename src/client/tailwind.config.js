b2679baab8f7a9d4690881935f30639afe4d16bc
Steve Warren <steve-warren@users.noreply.github.com>
2023-07-06 19:40:22 -0500

    add tailwindui

--- src/client/tailwind.config.js
+++ src/client/tailwind.config.js
@@ -1,11 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
     content: [
         "./index.html",
         "./**/*.{vue,js,ts,jsx,tsx}",
       ],
   theme: {
-    extend: {},
+      extend: {
+        fontFamily: {
+            sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+          },
+    },
   },
   plugins: [],
 }
